2be707d5ee5418fc9d02f75c662aac3d45155401
Celina Jure <celinajure@MacBook-Pro-von-Celina.local>
2022-10-17 19:41:14 -0300

    origin/master

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './componentes/error404/error404.component';
+import { IndexComponent } from './componentes/index/index.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { PanelComponent } from './componentes/panel/panel.component';
+import { RegistrateComponent } from './componentes/registrate/registrate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'panel', component: PanelComponent},
+  {path: 'registrate', component: RegistrateComponent},
+  {path: '**', component: Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { HardSkillsComponent } from './componentes/hard-skills/hard-skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { Error404Component } from './componentes/error404/error404.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { PanelComponent } from './componentes/panel/panel.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { IndexComponent } from './componentes/index/index.component';
+import { ModalComponent } from './modal/modal.component';
+import { RegistrateComponent } from './componentes/registrate/registrate.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SobreMiComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    HardSkillsComponent,
+    ProyectosComponent,
+    Error404Component,
+    FooterComponent,
+    PanelComponent,
+    LoginComponent,
+    IndexComponent,
+    ModalComponent,
+    RegistrateComponent
   ],
   imports: [
     BrowserModule,
